26f0ef339ee75393bb6ab39643d7bc03a234b061
Eitarou Futakuchi <>
2019-07-10 09:23:35 +0900

    とりあえずテキスト分は完成

--- app/pages/posts/_id.vue
+++ app/pages/posts/_id.vue
@@ -7,6 +7,30 @@
           <small>by {{ post.user.id }}</small>
         </div>
         <p>{{ post.body }}</p>
+        <no-ssr>
+          <p class="text-right">
+            <el-button
+              v-if="isLiked"
+              :disabled="!isLoggedIn"
+              type="warning"
+              round=""
+              @click="unlike"
+            >
+              <span class="el-icon-star-on" />
+              <span>{{ post.likes.length }}</span>
+            </el-button>
+            <el-button
+              v-else
+              :disabled="!isLoggedIn"
+              type="warning"
+              round=""
+              @click="like"
+            >
+              <span class="el-icon-star-off" />
+              <span>{{ post.likes.length }}</span>
+            </el-button>
+          </p>
+        </no-ssr>
         <p class="text-right">{{ post.created_at | time }}</p>
       </el-card>
       <p>
@@ -18,36 +42,47 @@
 
 <script>
 import moment from '~/plugins/moment';
-import { mapGetters } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
+import cloneDeep from 'lodash.clonedeep';
 export default {
   filters: {
     time(val) {
       return moment(val).format('YYYY/MM/DD HH:mm:ss');
     },
   },
-  async asyncData({ store, route, error }) {
-    const { id } = route.params;
-    if (store.getters['posts/posts'].find((p) => p.id === id)) {
+  async asyncData({ store, route }) {
+    if (store.getters['posts/posts'].find((p) => p.id === route.params.id)) {
       return;
     }
-    try {
-      await store.dispatch('posts.fetchPost', { id });
-      if (
-        !store.getters['posts/posts'].find(
-          (p) => p.id === this.$route.params.id,
-        )
-      ) {
-        throw new Error('post not found');
-      }
-    } catch (e) {
-      error({ statusCode: 404 });
-    }
+    await store.dispatch('posts/fetchPosts');
   },
   computed: {
     post() {
       return this.posts.find((p) => p.id === this.$route.params.id);
     },
+    isLiked() {
+      if (!this.user) return false;
+      return this.post.likes.find((l) => l.user_id === this.user.id);
+    },
+    ...mapGetters(['user', 'isLoggedIn']),
     ...mapGetters('posts', ['posts']),
+  },
+  methods: {
+    like() {
+      if (!this.isLoggedIn) {
+        return;
+      }
+      const likePayload = { user: this.user, post: this.post };
+      this.addLikeToPost(cloneDeep(likePayload));
+      this.addLikeLogToUser(cloneDeep(likePayload));
+    },
+    unlike() {
+      if (!this.isLoggedIn) {
+        return;
+      }
+    },
+    ...mapActions(['addLikeLogToUser']),
+    ...mapActions('posts', ['addLikeToPost']),
   },
 };
 </script>
